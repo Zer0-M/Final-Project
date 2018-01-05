20ef591f7190c0aa2ee4e38d56d13df96f515ee7
Zer0-M <mjamil90@stuy.edu>
2018-01-04 19:12:12 -0500

    added fields and function signatures

--- Tetris.java
+++ Tetris.java
@@ -1,3 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 public class Tetris extends JFrame implements ActionListener{
+  private Container pane;
+  private JTextField score;
+  private Board matrix;
+  public Tetris(){
 
+  }
+  public int getScore(){
+
+  }
 }
