07e6cc1421060aa59d573622f8eafd4d6219bc40
Zer0-M <mjamil90@stuy.edu>
2018-01-04 19:24:18 -0500

    added layout and dimensions for window and filled getScore method

--- Tetris.java
+++ Tetris.java
@@ -6,9 +6,15 @@ public class Tetris extends JFrame implements ActionListener{
   private JTextField score;
   private Board matrix;
   public Tetris(){
+    this.setTitle("Tetris");
+    this.setSize(500,250);
+    this.setLocation(100,100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+    pane = this.getContentPane();
+    pane.setLayout(new FlowLayout());
   }
   public int getScore(){
-
+    return score.getText();
   }
 }
