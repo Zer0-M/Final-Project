b9c94fad5d0ea0887dd818dbb9307dab09a70ec6
Zer0-M <mjamil90@stuy.edu>
2018-01-06 23:17:51 -0500

    added background color to board

--- Board.java
+++ Board.java
@@ -4,14 +4,16 @@ import java.awt.event.*;
 import java.awt.Graphics;
 import java.awt.Color;
 public class Board extends JPanel{
-    public Board(){
-    }
-    public void paint(Graphics g){
-	g.drawRect(50,50,20,20);
-	g.drawRect(49,49,20,20);
-	g.drawRect(50,50,300,500);
-	g.setColor(Color.RED);
-	g.fillRect(50,50,20,20);
-    }
+	public Board() {
+		setBackground(Color.WHITE);
+    setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
+  public void paint(Graphics g){
+    super.paintComponent(g);
+    g.drawRect(0,0,20,20);
+    g.drawRect(-1,-1,20,20);
+    g.setColor(Color.RED);
+    g.fillRect(0,0,20,20);
+  }
 }
 	
