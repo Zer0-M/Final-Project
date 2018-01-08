d386bf9562cbebdbefc97431402b24cb5db43800
Imad <ibelkebir@stuy.edu>
2018-01-08 01:39:04 -0500

    Added semi-random shape generation

--- Board.java
+++ Board.java
@@ -12,22 +12,24 @@ public class Board extends JPanel{
 	Tetrimino t = new Tetrimino();
 	int row = 0;
 	int col = 0;
+	int shape = 0;
 	for(int x=0; x<7; x++){
+	    shape=t.randGen();
 	    for(int i=1; i<3; i++){
-		for(int j=0; j<t.getLen(x); j++){
-		    if(t.getCoor(x,i,j) == 1){
+		for(int j=0; j<t.getLen(shape); j++){
+		    if(t.getCoor(shape,i,j) == 1){
 			g.drawRect(row,col,20,20);
 			g.drawRect(row-1,col-1,20,20);
-			g.setColor(t.getCol(x));
+			g.setColor(t.getCol(shape));
 			g.fillRect(row,col,20,20);
 			g.setColor(Color.BLACK);
 		    }
-		    col += 20;
+		    row += 20;
 		}
-		row += 20;
-		col = 0;
+		col += 20;
+		row = 0;
 	    }
-	    row += 10;
+	    col += 10;
 	}
     }
 }
--- Tetrimino.java
+++ Tetrimino.java
@@ -3,9 +3,13 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Tetrimino{
     private int[][][] shapes;
     private Color[] colors;
+    private ArrayList<Integer> shapeGen;
+    private Integer[] newGen;
     public Tetrimino(){
 	colors = new Color[] {Color.CYAN,Color.YELLOW,Color.MAGENTA,Color.GREEN,Color.RED,Color.BLUE,Color.ORANGE};
 	shapes = new int[][][] {
@@ -38,6 +42,8 @@ public class Tetrimino{
 	     {1,1,1}
 	    }
 	};
+	newGen = new Integer[] {0,0,1,1,2,2,3,3,4,4,5,5,6,6};
+	shapeGen = new ArrayList<Integer> ();
     }
     public int getCoor(int shape, int x, int y){
 	return shapes[shape][x][y];
@@ -47,6 +53,12 @@ public class Tetrimino{
     }
     public Color getCol(int shape){
 	return colors[shape];
+    }
+    public int randGen(){
+	if(shapeGen.size() == 0){
+	    shapeGen.addAll(Arrays.asList(newGen));
+	}
+	return shapeGen.remove((int)(Math.random()*shapeGen.size()));
     }
     public static void main(String[]args){
 	Tetrimino t = new Tetrimino();
