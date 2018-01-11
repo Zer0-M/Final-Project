172559acf4cab9ce76563a585af2fa620ec15dae
Zer0-M <mjamil90@stuy.edu>
2018-01-11 01:27:43 -0500

    added buttons that pause and start the game

--- Tetris.java
+++ Tetris.java
@@ -3,9 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.Graphics;
 import java.awt.Color;
-public class Tetris extends JFrame{
+public class Tetris extends JFrame implements ActionListener{
   private Container pane;
   private JLabel  score;
+  private JButton pause;
+  private JButton play;
   private Board  matrix;
   private JPanel sidebar;
   public Tetris(){
@@ -13,16 +15,31 @@ public class Tetris extends JFrame{
     this.setSize(800,800);
     this.setLocation(100,100);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+    pause=new JButton("pause");
+    play=new JButton("play");
+    pause.addActionListener(this);
+    play.addActionListener(this);
     pane = this.getContentPane();
     pane.setLayout(new GridLayout());
     score=new JLabel("0");
     sidebar=new JPanel();
+
     score.setFont(new Font("Serif",Font.PLAIN,30));
     matrix=new Board();
     pane.add(matrix);
     pane.add(sidebar);
+    sidebar.add(play);
+    sidebar.add(pause);
     sidebar.add(score);
+  }
+  public void actionPerformed(ActionEvent e){
+    String s=e.getActionCommand();
+    if(s.equals("pause")){
+      matrix.pause();
+    }
+    else if(s.equals("play")){
+      matrix.play();
+    }
   }
   public String getScore(){
     return score.getText();
